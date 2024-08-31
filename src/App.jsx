95adf3833c9175fc69c7778f8bfa9e0054127c22
MUHAMMED MAHIR K <mahirkambran@gmail.com>
2024-09-01 01:09:53 +0530

    iPhone 15 clone site completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -8,6 +12,10 @@ export default function App() {
       <Navbar />
       <Hero />
       <Highlights/>
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
